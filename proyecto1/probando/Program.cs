6e9f479ab72fb042add951bff18612eef98faf98
Alejandro Curiel <alex_curielg@hotmail.com>
2026-03-05 16:50:05 -0600

    agregar ;

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Playzi!");
 
-string stripe_api_key="sk_test_51N8n9uKj2mLh7sQqj"
+string stripe_api_key="sk_test_51N8n9uKj2mLh7sQqj";
